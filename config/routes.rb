02781f3141ab8bba696c0e3265e81b5282061bdd
Jeff <jwmorrow1s@gmail.com>
2015-04-14 15:38:01 -0500

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
-
+  get 'static_pages/about'
   get 'static_pages/help'
 
-  root :to => 'application#hello'
+  root :to => 'static_pages#home'
 end
